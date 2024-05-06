f4b03b84f3aeaa114ee19f6b166768801b80c10d
AkideweKuseh <michaelakidewe@gmail.com>
2024-05-06 18:10:55 +0000

    Saving Progress

--- Section 1/app.js
+++ Section 1/app.js
@@ -2,7 +2,9 @@ const vm = Vue.createApp({
     data(){
         return{
             firstName: 'Millicent',
-            lastName: 'Benson'
+            lastName: 'Benson',
+            url:'https://www.google.com',
+            raw_url: '<a href="https://www.google.com" target="_blank">Google🔍</a>'
         }
     },
 
