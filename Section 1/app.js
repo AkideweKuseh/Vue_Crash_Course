25c07a2c5de132ae4001c615c3b5a763b99df760
AkideweKuseh <michaelakidewe@gmail.com>
2024-05-06 00:57:03 +0000

    Saving Progress

--- Section 1/app.js
+++ Section 1/app.js
@@ -1,1 +1,28 @@
-Vue.createApp({}).mount('#app')
+const vm = Vue.createApp({
+    data(){
+        return{
+            firstName: 'Millicent',
+            lastName: 'Benson'
+        }
+    },
+
+    methods: {
+        fullName(){
+            return `${this.firstName}  ${this.lastName.toUpperCase()}`
+        }
+    }
+}).mount('#app')
+
+// setTimeout(() => {
+//     vm.firstName = 'Bob';
+// }, 2000);
+
+
+// Vue.createApp({
+//     data(){
+//         return{
+//             firstName: 'John',
+//             lastName: 'Kofi'
+//         }
+//     }
+// }).mount('#vapp')
