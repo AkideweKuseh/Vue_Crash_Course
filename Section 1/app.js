7f916550bad0dabcc78a4a3ae300c97161de14ff
AkideweKuseh <michaelakidewe@gmail.com>
2024-05-08 20:52:44 +0000

    Saving Changes

--- Section 1/app.js
+++ Section 1/app.js
@@ -1,16 +1,31 @@
 const vm = Vue.createApp({
     data(){
         return{
-            firstName: 'Millicent',
-            lastName: 'Benson',
+            firstName: 'John',
+            middleName: '',
+            lastName: 'Doe',
             url:'https://www.google.com',
-            raw_url: '<a href="https://www.google.com" target="_blank">Google🔍</a>'
+            raw_url: '<a href="https://www.google.com" target="_blank">Google🔍</a>',
+            age: 10
         }
     },
 
     methods: {
         fullName(){
-            return `${this.firstName}  ${this.lastName.toUpperCase()}`
+            return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
+        },
+        increment(){
+            this.age += 1;
+        },
+        decrement(){
+            this.age -= 1;
+        },
+        updateLastName(msg,event){
+            console.log(msg)
+            this.lastName = event.target.value
+        },
+        updateMiddleName(event){
+            this.middleName = event.target.value
         }
     }
 }).mount('#app')
