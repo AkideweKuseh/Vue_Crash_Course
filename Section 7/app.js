e274a8dbb55cd3f8077169660a9cf70465f17b7f
AkideweKuseh <michaelakidewe@gmail.com>
2024-05-18 15:23:18 +0000

    Saving Progess

--- Section 7/app.js
+++ Section 7/app.js
@@ -3,5 +3,35 @@ let vm = Vue.createApp({
     return {
       message: "Hello world!"
     }
+  },
+  beforeCreate(){
+    console.log('beforeCreate() function has been called', this.message)
+  },
+  created(){
+    console.log('create() function has been called', this.message)
+  },
+  beforeMount(){
+    console.log('beforeMount() function has been called', this.$el)
+  },
+  mounted(){
+    console.log('mounted() function has been called', this.$el)
+  },
+  beforeUpdate(){
+    console.log('beforeUpdated() function has been called')
+  },
+  updated(){
+    console.log('updated() function has been called')
+  },
+  beforeUnmount(){
+    console.log('beforeUnmount() function has been called')
+  },
+  unmounted(){
+    console.log('unmounted() function has been called')
   }
-}).mount('#app')
+})
+
+vm.mount('#app')
+
+// setTimeout(()=> {
+//   vm.mount('#app')
+// },3000)
